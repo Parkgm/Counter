87a80da8578bcdbc023bf7de12e41be327846748
D7703_13 <d7703_13@d7703-13ui-imac.local>
2018-03-20 14:52:22 +0900

    number counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var countNumber: UILabel!
+    
+    @IBAction func tapButton(_ sender: UIButton) {
+        time = time + 1
+        countNumber.text = "\(time)"
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        time = 0
+        countNumber.text="\(time)"
+    }
+    
 }
 
